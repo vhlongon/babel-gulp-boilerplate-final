5e32f7fd624cf946de78387528eec22a9c890f9a
Victor Hugo Longon <vhlongon@yahoo.com.br>
2016-10-27 11:48:54 +0200

    update all packages and styles task (with postcss and cssnano as a processor) and images (with png optimization)

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,18 +1,18 @@
 'use strict';
 
 import config      from '../config';
-import changed     from 'gulp-changed';
 import gulp        from 'gulp';
 import gulpif      from 'gulp-if';
 import imagemin    from 'gulp-imagemin';
 import browserSync from 'browser-sync';
+import pngquant    from 'imagemin-pngquant';
 
 gulp.task('images', function() {
-
   return gulp.src(config.images.src)
-    .pipe(changed(config.images.dest)) // Ignore unchanged files
-    .pipe(gulpif(global.isProd, imagemin())) // Optimize
+    .pipe(gulpif(global.isProd, imagemin({
+                 progressive: true,
+                 plugins: [pngquant({quality: '65-80'})]
+            }))) // Optimize
     .pipe(gulp.dest(global.isProd ? config.images.prodDest : config.images.dest))
     .pipe(browserSync.stream({ once: true }));
-
 });
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,32 +5,32 @@ import gulp         from 'gulp';
 import gulpif       from 'gulp-if';
 import sourcemaps   from 'gulp-sourcemaps';
 import sass         from 'gulp-sass';
-import cssnext      from 'gulp-cssnext';
 import handleErrors from '../util/handleErrors';
 import browserSync  from 'browser-sync';
 import autoprefixer from 'gulp-autoprefixer';
+import postcss      from 'gulp-postcss';
+import cssnano      from 'cssnano';
+
 
 gulp.task('styles', function () {
 
   const createSourcemap = !global.isProd || config.styles.prodSourcemap;
+  const processors = [cssnano];
 
   return gulp.src(config.styles.src)
     .pipe(gulpif(createSourcemap, sourcemaps.init()))
     .pipe(sass({
-      sourceComments: !global.isProd,
+      // sourceComments: !global.isProd,
       errLogToConsole: true,
       outputStyle: global.isProd ? 'compressed' : 'nested',
       includePaths: config.styles.sassIncludePaths
     }))
     .on('error', handleErrors)
-    //.pipe(autoprefixer('last 2 versions', '> 1%', 'ie 8'))
-    .pipe(cssnext({
-      compress: false
-    }))
-
+    .pipe(autoprefixer('last 2 versions', '> 1%', 'ie 8'))
+    .pipe(gulpif(global.isProd, postcss(processors)))
     .pipe(gulpif(
       createSourcemap,
-      sourcemaps.write( global.isProd ? './' : null ))
+      sourcemaps.write((global.isProd ? null : '/sourcemaps'),  {sourceRoot: './' }))
     )
     .pipe(gulp.dest(global.isProd ? config.styles.prodDest : config.styles.dest))
     .pipe(browserSync.stream({ once: true }));
